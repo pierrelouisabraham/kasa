5c39681bedc33da87a3c79cce0193b47712c57b8
Pierre <pierrelouis.abraham@gmail.com>
2023-07-21 18:49:20 +0200

    commit correction public

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import Banner from './composants/header';
 import UnderHeader from './composants/UnderHeader';
+import Homepage from './pages/json'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Banner/>
+  <Homepage/>
   <UnderHeader/>
   </BrowserRouter>
 );
